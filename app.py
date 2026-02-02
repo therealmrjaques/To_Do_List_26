3f8e8ce6667e1b93205316a404c0cfcb21a430d5
Colin Jaques <cjaques@barker.nsw.edu.au>
2026-02-02 12:42:19 +1100

    Created Front End of To-Do List

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+PRIORITIES = ["High", "Medium", "Low"]
+
+@app.route("/")
+def index():
+    return render_template("index.html", priorities = PRIORITIES)
+
+@app.route('/sort')
+def sort():
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
